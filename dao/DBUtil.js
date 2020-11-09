cf46e2d49d4bab2e42b551f6cdf11a0a91d420f4
noleafage <654205917@qq.com>
2020-11-10 03:37:24 +0800

    xiugaidao

--- dao/DBUtil.js
+++ dao/DBUtil.js
@@ -3,7 +3,7 @@ const mysql = require("mysql")
 
 function createConnection(){
     const connection = mysql.createConnection({
-    host: "127.0.0.1",
+    host: "192.168.1.40",
     port:"3306",
     user:"root",
     password:"950428yuan",
